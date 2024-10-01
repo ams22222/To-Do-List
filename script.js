8ed2646730946fa13331aec61c9f2db58ba30df8
Yan <ulnay2004@gmail.com>
2024-10-01 12:48:01 +0200

    Crear llista quant click

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let table = [];
+
+function createTable() {
+    const nametable = document.getElementById("nameA").value;
+    if (nametable) {
+        let tableA = {
+            name: nametable,
+            task: {}
+        };
+    }
+    else {
+        alert("Intrudueix un nombre valid per la taula");
+    }
+}
